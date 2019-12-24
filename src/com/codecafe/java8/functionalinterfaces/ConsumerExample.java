b881be40c2a8167b6a54cb47c6734a8dfd1a0089
Abhinav <Abhinav@HEISENBERG>
2019-12-24 19:33:48 +0530

    Initial commit with few programs for Java 8

---
+++ src/com/codecafe/java8/functionalinterfaces/ConsumerExample.java
@@ -0,0 +1,14 @@
+package com.codecafe.java8.functionalinterfaces;
+
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+	public static void main(String args[]) {
+
+		Consumer<String> c = (s) -> System.out.println(s.toUpperCase());
+
+		c.accept("java8");
+		
+	}
+}
