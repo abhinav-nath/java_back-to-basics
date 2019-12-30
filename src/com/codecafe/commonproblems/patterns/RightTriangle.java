28d3246b1da57d77620f6ba91b436b7a7a838433
Abhinav <Abhinav@HEISENBERG>
2019-12-31 01:52:29 +0530

    Added few Pattern programs and String problems

---
+++ src/com/codecafe/commonproblems/patterns/RightTriangle.java
@@ -0,0 +1,36 @@
+package com.codecafe.commonproblems.patterns;
+
+import java.util.Scanner;
+/*
+Enter a number : 6
+
+*
+**
+***
+****
+*****
+******
+
+*/
+
+public class RightTriangle {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a number : ");
+		int num = sc.nextInt();
+		System.out.println();
+		
+		for(int i=1; i<=num; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+
+}
---
+++ src/com/codecafe/commonproblems/strings/Palindrome.java
@@ -0,0 +1,43 @@
+package com.codecafe.commonproblems.strings;
+
+import java.util.Scanner;
+
+/*
+Enter a String :
+madam
+Is madam a palindrome? : true
+*/
+public class Palindrome {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a String :");
+		String input = sc.nextLine();
+
+		System.out.printf("Is %s a palindrome? : %b", input, isPalindrome(input));
+
+		sc.close();
+	}
+
+	private static boolean isPalindrome(String input) {
+
+		if(input == null || input.isEmpty()) {
+			return true;
+		}
+		
+		char[] array = input.toCharArray();
+		
+		StringBuilder sb = new StringBuilder(input.length());
+		
+		// Reverse the String
+		for(int i=input.length()-1; i>=0; i--) {
+			sb.append(array[i]);
+		}
+		
+		// Compare input String with Reversed String
+		return input.equals(sb.toString());
+	}
+
+}
