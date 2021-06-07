89b570b9fa5d954800acf5bb45b5fde079f41c84
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-06-07 23:42:19 +0530

    code formatting

--- src/com/codecafe/commonproblems/strings/DuplicateCharacters.java
+++ src/com/codecafe/commonproblems/strings/DuplicateCharacters.java
@@ -11,28 +11,28 @@ New String is : hhheeellllllooo wwwooorrrlllddd
 
 public class DuplicateCharacters {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter a String : ");
-		String inputString = sc.nextLine();
-		
-		char[] charArray = inputString.toCharArray();
-		
-		StringBuffer newString = new StringBuffer();
-		for(char c : charArray) {
-			if(c != ' ') {
-				for(int i=1; i<=3; i++) {
-					newString.append(c);
-				}
-			} else {
-				newString.append(' ');
-			}
-		}
-		
-		System.out.println("\nNew String is : " + newString.toString());
-		sc.close();
-	}
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a String : ");
+        String inputString = sc.nextLine();
+
+        char[] charArray = inputString.toCharArray();
+
+        StringBuffer newString = new StringBuffer();
+        for (char c : charArray) {
+            if (c != ' ') {
+                for (int i = 1; i <= 3; i++) {
+                    newString.append(c);
+                }
+            } else {
+                newString.append(' ');
+            }
+        }
+
+        System.out.println("\nNew String is : " + newString.toString());
+        sc.close();
+    }
 
 }
--- src/com/codecafe/commonproblems/strings/Palindrome.java
+++ src/com/codecafe/commonproblems/strings/Palindrome.java
@@ -9,35 +9,35 @@ Is madam a palindrome? : true
 */
 public class Palindrome {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter a String :");
-		String input = sc.nextLine();
+        System.out.println("Enter a String :");
+        String input = sc.nextLine();
 
-		System.out.printf("Is %s a palindrome? : %b", input, isPalindrome(input));
+        System.out.printf("Is %s a palindrome? : %b", input, isPalindrome(input));
 
-		sc.close();
-	}
+        sc.close();
+    }
 
-	private static boolean isPalindrome(String input) {
+    private static boolean isPalindrome(String input) {
 
-		if(input == null || input.isEmpty()) {
-			return true;
-		}
-		
-		char[] array = input.toCharArray();
-		
-		StringBuilder sb = new StringBuilder(input.length());
-		
-		// Reverse the String
-		for(int i=input.length()-1; i>=0; i--) {
-			sb.append(array[i]);
-		}
-		
-		// Compare input String with Reversed String
-		return input.equals(sb.toString());
-	}
+        if (input == null || input.isEmpty()) {
+            return true;
+        }
+
+        char[] array = input.toCharArray();
+
+        StringBuilder sb = new StringBuilder(input.length());
+
+        // Reverse the String
+        for (int i = input.length() - 1; i >= 0; i--) {
+            sb.append(array[i]);
+        }
+
+        // Compare input String with Reversed String
+        return input.equals(sb.toString());
+    }
 
 }
