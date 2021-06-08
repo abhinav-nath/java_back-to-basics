af8500feb0c863df400b8a1ebe201f40d960e4fc
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-06-08 14:47:36 +0530

    added explanation about island of isolation

--- src/com/codecafe/corejava/garbage_collection/IslandOfIsolation.java
+++ src/com/codecafe/corejava/garbage_collection/IslandOfIsolation.java
@@ -1,5 +1,17 @@
 package com.codecafe.corejava.garbage_collection;
 
+/*
+    Object 1 references Object 2 and Object 2 references Object 1.
+    Neither Object 1 nor Object 2 is referenced by any other object.
+    
+    That's an island of isolation.
+    
+    Basically, an island of isolation is a group of objects that reference each other but
+    they are not referenced by any active object in the application.
+    
+    Strictly speaking, even a single unreferenced object is an island of isolation too.
+ */
+
 class Test {
     Test i;
 
